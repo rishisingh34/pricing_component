cf418aca0dbbeb58b4944766b3693077d393e69c
rishisingh34 <singh34rishi@gmail.com>
2024-10-18 12:24:34 +0530

    redesigned inner card component - added style to slider's thumb - add billing slider and footer for inner card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="w-screen h-screen flex justify-center items-center">
           {/* <Image src={"/images/icon-check.svg"} alt="check" width={100} height={100}/>
           <Image src={"/images/favicon-32X32.png"} alt="check" width={100} height={100}/>
-          <Image src={"/images/icon-slider.svg"} alt="check" width={100} height={100}/>
+          
           <Image src={"/images/pattern-circles.svg"} alt="check" width={100} height={100}/> */}
           <Pricing />
       </div>
